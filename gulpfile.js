57d4381ef07262a01deeb282d4c92880f493fbbd
Tobias Reich <tobias.reich.ich@gmail.com>
2016-11-04 21:01:28 +0100

    Description

--- gulpfile.js
+++ gulpfile.js
@@ -9,17 +9,10 @@ const scripts = tasks.scripts({
 	to   : './dist'
 })
 
-const styles = tasks.styles({
-	from : './src/styles/main.scss',
-	to   : './dist'
-})
-
 const watch = function() {
 	gulp.watch('./src/scripts/**/*.js', [ 'scripts' ])
-	gulp.watch('./src/styles/**/*.scss', [ 'styles' ])
 }
 
 gulp.task('scripts', scripts)
-gulp.task('styles', styles)
-gulp.task('default', [ 'scripts', 'styles' ])
+gulp.task('default', [ 'scripts' ])
 gulp.task('watch', [ 'default' ], watch)
